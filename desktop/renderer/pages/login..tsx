ac6ff84675827421fe31c348a4a69faa53a05d9a
Mutesa-Cedric <mutesacedric@gmail.com>
2023-06-29 15:32:06 +0200

    update(desktop) : login page

--- desktop/renderer/pages/login..tsx
+++ desktop/renderer/pages/login..tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-import { AuthLayout } from '@/components/AuthLayout'
 import { Button } from '@/components/Button'
 import { TextField } from '@/components/Fields'
-// import { Logo } from '@/components/Logo'
 import { useRouter } from 'next/router'
 import { FormEvent, useState } from 'react'
+import Logo from '@/components/logo'
+import { AuthLayout } from '@/components/layouts/AuthLayout'
+import Loader from '@/components/Loader'
 
 const admin = {
     email: 'voiptradmin@gmail.com',
@@ -18,24 +19,21 @@ const normalUser = {
     password: '323232'
 }
 export default function Login() {
+    const [loading, setLoading] = useState(false);
 
     const router = useRouter();
     const [formData, setFormData] = useState({
         email: "",
         password: "",
-        serialNumber:''
+        serialNumber: ''
     });
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (formData.email == normalUser.email && formData.password == normalUser.password) {
-            router.push('/u')
-        }
-        else if (formData.email == admin.email && formData.password == admin.password) {
-            router.push('/a')
-        }
-        else {
-            alert('Wrong email or password')
-        }
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+            // router.push('/dashboard'
+        }, 2000)
     }
     return (
         <>
@@ -45,13 +43,13 @@ export default function Login() {
             <AuthLayout>
                 <div className="flex flex-col">
                     <Link href="/" aria-label="Home">
-                        {/* <Logo className="h-10 w-auto" /> */}
+                        <Logo className="h-10 w-auto" />
                     </Link>
                     <div className="mt-20">
                         <h2 className="text-lg font-semibold text-gray-900">
                             Sign in to your account
                         </h2>
-                        <p className="mt-2 text-sm text-gray-700">
+                        {/* <p className="mt-2 text-sm text-gray-700">
                             Don’t have an account?{' '}
                             <Link
                                 href="/register"
@@ -60,7 +58,7 @@ export default function Login() {
                                 Sign up
                             </Link>{' '}
                             for a free trial.
-                        </p>
+                        </p> */}
                     </div>
                 </div>
                 <form onSubmit={(e) => handleSubmit(e)} className="mt-10 grid grid-cols-1 gap-y-8">
@@ -99,10 +97,11 @@ export default function Login() {
                             variant="solid"
                             color="blue"
                             className="w-full"
-                        >
+                        >{loading ? <Loader /> :
                             <span>
                                 Sign in <span aria-hidden="true">&rarr;</span>
                             </span>
+                            }
                         </Button>
                     </div>
                 </form>
