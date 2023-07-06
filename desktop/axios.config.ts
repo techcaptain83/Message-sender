eff99402b414a5d0d2c726d842bb2e124e429e6f
Mutesa-Cedric <mutesacedric@gmail.com>
2023-07-06 17:58:52 +0200

    update : switched to prod backend url

--- desktop/axios.config.ts
+++ desktop/axios.config.ts
@@ -5,7 +5,8 @@ const isDev = process.env.NODE_ENV === "development";
 const DEV_URL = "http://localhost:8000";
 const PROD_URL = "https://chatmaid.onrender.com"
 const axiosInstance = axios.create({
-    baseURL: isDev ? DEV_URL : PROD_URL,
+    // baseURL: isDev ? DEV_URL : PROD_URL,
+    baseURL: PROD_URL,
 });
 
 export default axiosInstance;
