0ff182fe5a3d63dde47b73d06384a24b15fc2bb5
Mutesa-Cedric <mutesacedric@gmail.com>
2023-07-06 17:57:48 +0200

    chore : changed remote repo

--- desktop/renderer/components/home/navbar.tsx
+++ desktop/renderer/components/home/navbar.tsx
@@ -11,9 +11,14 @@ export default function Navbar() {
 
     return (
         <div className='w-full py-4 bg-gray-50/75 flex justify-between items-center  shadow px-8 h-[9vh]'>
-            <Link href={'/'}>
-                <Logo className="h-10" />
-            </Link>
+            <div className='flex items-center gap-3'>
+                <Link href={'/'}>
+                    <Logo className="h-10" />
+                </Link>
+                {/* <Link href={'/history'}>
+                    <button className='rounded-md text-gray-700 font-medium hover:text-blue-500  p-2 text-sm' >Message History</button>
+                </Link> */}
+            </div>
             <div className='flex items-center gap-6 '>
                 <div className='flex gap-3 items-center '>
                     <div className='w-10 h-10 rounded-full flex items-center bg-blue-500 justify-center font-medium text-white text-sm '>
