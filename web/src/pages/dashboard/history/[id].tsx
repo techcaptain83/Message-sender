27cab735d111a478aae17e1868befcb2fb69e155
Mutesa-Cedric <mutesacedric@gmail.com>
2023-08-11 19:28:00 +0200

    update : logs page

--- web/src/pages/dashboard/history/[id].tsx
+++ web/src/pages/dashboard/history/[id].tsx
@@ -3,6 +3,7 @@ import { ILog } from '@/types';
 import axios from '@/axios.config';
 import { saveAs } from 'file-saver';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 interface Props {
     log: ILog
 }
@@ -52,22 +53,27 @@ export default function LogView({ log }: Props) {
     }
 
     return (
-        <div className='w-full h-[91vh px-8 py-4'>
-            <div className='w-full flex items-center justify-between'>
-                <h1 className="text-base font-medium leading-6 text-gray-900">Logs</h1>
-                <button
-                    onClick={downloadLogs}
-                    type="button"
-                    className="block rounded-md  bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 capitalize"
-                >
-                    Download Logs
-                </button>
+        <>
+        <Head>
+            <title>Chatmaid - Logs</title>
+        </Head>
+            <div className='w-full h-[91vh px-8 py-4'>
+                <div className='w-full flex items-center justify-between'>
+                    <h1 className="text-base font-medium leading-6 text-gray-900">Logs</h1>
+                    <button
+                        onClick={downloadLogs}
+                        type="button"
+                        className="block rounded-md  bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 capitalize"
+                    >
+                        Download Logs
+                    </button>
+                </div>
+                <p className='pt-3 text-gray-600 max-w-4xl'>
+                    Logs for Messages sent to list <span className='text-gray-800 font-semibold'>{log.filename} </span>
+                    Due on <span className='text-gray-800 font-semibold'>{new Date(log.createdAt).toLocaleString()}</span>
+                </p>
+                <LogsTable contacts={log.contacts} />
             </div>
-            <p className='pt-3 text-gray-600 max-w-4xl'>
-                Logs for Messages sent to list <span className='text-gray-800 font-semibold'>{log.filename} </span>
-                Due on <span className='text-gray-800 font-semibold'>{new Date(log.createdAt).toLocaleString()}</span>
-            </p>
-            <LogsTable contacts={log.contacts} />
-        </div>
+        </>
     )
 }
