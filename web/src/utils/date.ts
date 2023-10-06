dcb0c7f6f793491143eaff5ae23d784931d64136
Mutesa-Cedric <mutesacedric@gmail.com>
2023-10-06 09:44:44 +0200

    feat : date conversions

--- web/src/utils/date.ts
+++ web/src/utils/date.ts
@@ -1,3 +1,5 @@
+import moment from 'moment-timezone';
+
 export const generateStartsAndEndsAtDate = (date: string, hour: string, slot: { starts: number, ends: number }): { startsAt: Date, endsAt: Date } => {
    const formattedHour = hour.padStart(2, '0'); // Ensure two-digit format
    const formattedStarts = slot.starts.toString().padStart(2, '0'); // Ensure two-digit format
@@ -12,19 +14,27 @@ export const generateStartsAndEndsAtDate = (date: string, hour: string, slot: { 
 }
 
 
-export const generateStartsAndEndsAtDateWithOffset = (date: string, hour: string, slot: { starts: number, ends: number }): { startsAt: Date, endsAt: Date } => {
-   const formattedHour = hour.padStart(2, '0'); // Ensure two-digit format
-   const formattedStarts = slot.starts.toString().padStart(2, '0'); // Ensure two-digit format
+export const convertToUTC = (startsAt: Date, endsAt: Date): { startsAt: Date, endsAt: Date } => {
+   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-   const startsAt = new Date(`${date}T${formattedHour}:${formattedStarts}:00.000Z`);
-   const endsAt = new Date(`${date}T${formattedHour}:${slot.ends - 1}:59.999Z`);
-
-   const offset = startsAt.getTimezoneOffset();
-   startsAt.setMinutes(startsAt.getMinutes() - offset);
-   endsAt.setMinutes(endsAt.getMinutes() - offset);
+   const startsAtUTC = moment.tz(startsAt, timeZone).utc().toDate();
+   const endsAtUTC = moment.tz(endsAt, timeZone).utc().toDate();
 
    return {
-      startsAt,
-      endsAt
+      startsAt: startsAtUTC,
+      endsAt: endsAtUTC
+   }
+}
+
+export const convertToUserTimezone = (startsAt: Date, endsAt: Date): { startsAt: Date, endsAt: Date } => {
+
+   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+   const startsAtLocal = moment.tz(startsAt, timeZone).toDate();
+   const endsAtLocal = moment.tz(endsAt, timeZone).toDate();
+
+   return {
+      startsAt: startsAtLocal,
+      endsAt: endsAtLocal
    }
 }
